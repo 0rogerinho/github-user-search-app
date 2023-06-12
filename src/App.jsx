7b575de772369caae216e8245b4b6a6dcb97c7ad
0rogerinho <luaansilva11@gmail.com>
2023-06-11 20:47:17 -0500

    not bugs two

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import UserProfile from './components/UserProfile';
 import ThemeProvider from './components/Context/ThemeProvider';
-import Layout from './components/layout';
+import Layout from './components/Layout';
 import UserData from './components/Context/UserData';
 function App() {
   return (
